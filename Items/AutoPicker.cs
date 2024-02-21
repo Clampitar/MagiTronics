97aa01455e458f9dfe739ebf17a488d3832771ae
John Muller <johnmuller575@gmail.com>
2024-02-20 19:01:42 -0500

    Added AutoPicker

---
+++ Items/AutoPicker.cs
@@ -0,0 +1,13 @@
+﻿using Terraria.ModLoader;
+
+namespace MagiTronics.Items
+{
+    internal class AutoPicker : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.DefaultToPlaceableTile(ModContent.TileType<Tiles.AutoPicker>());
+            Item.mech = true;
+        }
+    }
+}
---
+++ Tiles/AutoPicker.cs
@@ -0,0 +1,25 @@
+﻿using Terraria.ModLoader;
+using Terraria;
+using Terraria.ObjectData;
+
+namespace MagiTronics.Tiles
+{
+    internal class AutoPicker : ModTile
+    {
+        public override void SetStaticDefaults()
+        {
+
+            Main.tileFrameImportant[Type] = true;
+            Main.tileNoAttach[Type] = true;
+
+            TileObjectData.newTile.CopyFrom(TileObjectData.Style3x3);
+            TileObjectData.newTile.LavaDeath = false;
+            TileObjectData.addTile(Type);
+        }
+
+        public override void HitWire(int i, int j)
+        {
+            base.HitWire(i, j);
+        }
+    }
+}
